244fc7b9c92baf7491a7b31a9872e5b4ae83a67e
germygrimes <germygrimes@gmail.com>
2022-01-21 18:58:34 -0500

    hey

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+
+OJavaQuestions/app/src/test/java/com/example/activities/one/ActivityOneTest.java,f\9\f9515c8d04ebe0c88c7c6f71f3ec45153d2bbf0c
+s
+CJavaQuestions/app/src/main/java/com/example/activities/one/Foo.java,4\0\407f3b990ea8a674552cedd811b54f7bfeb4080b
--- JavaQuestions/app/src/main/java/com/example/activities/five/ArraysActivity.java
+++ JavaQuestions/app/src/main/java/com/example/activities/five/ArraysActivity.java
@@ -1,31 +1,62 @@
 package com.example.activities.five;
 
 //Uncomment the code to start the activity
-/*
+
 public class ArraysActivity {
 
-    public int[] squares(int ... a) {
+    public int[] squares(int ... a) { //same as passing in (int a1, int a2, int a3)
         //Given any number of inputs, create an array the correct size
         //Store the square of every number in the array
 
+        int[] square = new int[a.length];
+        String sq = "";
+
+        for (int j = 0; j < a.length; j++){
+            square[j] = ((a[j])*(a[j]));
+            sq = sq + Integer.toString(((a[j])*(a[j]))) + ", ";
+        }
 
         //This should be modified to return the correct array
-        return new int[0];
+        System.out.println("the array's squares are: " + sq);
+        return square;
     }
 
     public int arraySum(int[] nums) {
         //Return the sum of the numbers inside of the given array
+        int sum = 0;
+
+        for (int j = 0; j < nums.length; j++){
+            sum = (sum + nums[j]);
+        }
 
         //This should be modifed to return the correct number
-        return 0;
+        System.out.println("the array's sum is: " + Integer.toString(sum));
+        return sum;
     }
 
     public double arrayAverage(double[] nums) {
         //Return the average of the numbers given in the array
 
+        double sum = 0;
+
+        for (int j = 0; j < nums.length; j++){
+            sum = sum + nums[j];
+        }
+
         //This should be modified to return the correct number
-        return 0.0;
+        System.out.println("the array's average is: " + Double.toString((sum/(nums.length))));
+        return (sum/(nums.length));
+    }
+
+    public static void main(String args[]) {
+
+        ArraysActivity arr = new ArraysActivity();
+        arr.squares(5, 4, 9, 3, 6, 1);
+        int[] a = {5, 4, 9, 3, 6, 1};
+        arr.arraySum(a);
+        double[] d = {1.0, 3.4, 5.6, 99.0, 88.6};
+        arr.arrayAverage(d);
+
     }
 
 }
- */
