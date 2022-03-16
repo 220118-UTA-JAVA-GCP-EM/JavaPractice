ef8a5ec65a2b4d01678a9790e885b63f5f2c4d84
Ryan Sy <ryansy@Ryans-Air.fios-router.home>
2022-03-16 02:50:56 -0400

    3 done

--- JavaQuestions/app/src/main/java/com/example/activities/three/WrapperActivity.java
+++ JavaQuestions/app/src/main/java/com/example/activities/three/WrapperActivity.java
@@ -1,21 +1,20 @@
 package com.example.activities.three;
+
 //Uncomment the code to start this activity
-/*
 public class WrapperActivity {
 
     Integer convertToInteger(String num1, String num2){
         //Given the string above, convert them to integers, and return the sum
-
+        int x = Integer.parseInt(num1);
+        int y = Integer.parseInt(num2);
         //Change the return to be correct
-        return 0;
+        return x + y;
     }
 
     Integer maxValue(){
         //Return the max value of an integer
 
-        return 0;
+        return Integer.MAX_VALUE;
     }
 
 }
-
- */
